6c27430826ac9a8e6661bbc7907ba5fdc61ed39e
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-10-16 16:10:36 +0200

    Add test_console

---
+++ tests/test_console.py
@@ -0,0 +1,14 @@
+import click.testing
+import pytest
+
+from hypermodern_python import console
+
+
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
+
+
+def test_main_succeeds(runner):
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
