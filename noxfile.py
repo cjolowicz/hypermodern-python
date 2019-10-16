548bfd4d122f4ba18a59336b37c0a661c65bd5cf
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-10-16 13:14:05 +0200

    Add lint session to noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,16 @@
 import nox
 
 
+locations = "src", "tests", "noxfile.py"
+
+
+@nox.session(python=["3.8", "3.7"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
+
+
 @nox.session(python=["3.8", "3.7"])
 def tests(session):
     args = session.posargs or ["--cov"]
