536e69f77f96c615afd559a4aa425416ae1611be
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-08 19:56:23 +0100

    Add test case for console.run

--- tests/test_console.py
+++ tests/test_console.py
@@ -19,3 +19,8 @@ def mock_splines_reticulate(mocker):
 def test_main_succeeds(runner, mock_splines_reticulate):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
+
+
+def test_main_prints_progress_message(runner, mock_sleep):
+    result = runner.invoke(console.main, ["--count=1"])
+    assert result.output == "Reticulating spline 1...\n"
