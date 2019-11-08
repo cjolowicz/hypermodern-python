f245c190afca1b18e9cd347835e0c4a2a8be4fbd
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-08 22:52:53 +0100

    Mock splines.reticulate in console test

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,6 +9,13 @@ def runner():
     return click.testing.CliRunner()
 
 
-def test_main_succeeds(runner):
+@pytest.fixture
+def mock_splines_reticulate(mocker):
+    mock = mocker.patch("hypermodern_python.splines.reticulate")
+    mock.return_value = [1, 2, 3]
+    return mock
+
+
+def test_main_succeeds(runner, mock_splines_reticulate):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
