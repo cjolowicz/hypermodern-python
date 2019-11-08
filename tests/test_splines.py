152370f3515e2270f89cecb72f6dbf8f1faa8002
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-08 19:59:25 +0100

    Use time.sleep mock in splines test

--- tests/test_splines.py
+++ tests/test_splines.py
@@ -1,6 +1,6 @@
 from hypermodern_python import splines
 
 
-def test_reticulate_yields_count_times():
+def test_reticulate_yields_count_times(mock_sleep):
     iterator = splines.reticulate(3)
     assert sum(1 for _ in iterator) == 3
