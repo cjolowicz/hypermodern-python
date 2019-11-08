6d83322f857f3f5dba3ab6068a298abd2f8c836d
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-08 20:25:56 +0100

    Add another test case for splines.reticulate

--- tests/test_splines.py
+++ tests/test_splines.py
@@ -4,3 +4,9 @@ from hypermodern_python import splines
 def test_reticulate_yields_count_times(mock_sleep):
     iterator = splines.reticulate(3)
     assert sum(1 for _ in iterator) == 3
+
+
+def test_reticulate_sleeps(mock_sleep):
+    for _ in splines.reticulate():
+        break
+    assert mock_sleep.called
