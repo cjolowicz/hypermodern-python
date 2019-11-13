9b445dff2132457f9af700398523fb6049091d90
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-13 13:44:43 +0100

    Add docstrings to test suite

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+"""Package-wide test fixtures."""
 from unittest.mock import Mock
 
 import pytest
@@ -6,4 +7,5 @@ from pytest_mock import MockFixture
 
 @pytest.fixture
 def mock_sleep(mocker: MockFixture) -> Mock:
+    """Mock for time.sleep."""
     return mocker.patch("time.sleep")
--- tests/test_console.py
+++ tests/test_console.py
@@ -1,3 +1,4 @@
+"""Test cases for the console module."""
 from unittest.mock import Mock
 
 from click.testing import CliRunner
@@ -9,21 +10,25 @@ from hypermodern_python import console
 
 @pytest.fixture
 def runner() -> CliRunner:
+    """Fixture for invoking command-line interfaces."""
     return CliRunner()
 
 
 @pytest.fixture
 def mock_splines_reticulate(mocker: MockFixture) -> Mock:
+    """Mock for splines.reticulate."""
     mock = mocker.patch("hypermodern_python.splines.reticulate")
     mock.return_value = [1, 2, 3]
     return mock
 
 
 def test_main_succeeds(runner: CliRunner, mock_splines_reticulate: Mock) -> None:
+    """Test if console.main succeeds."""
     result = runner.invoke(console.main)
     assert result.exit_code == 0
 
 
 def test_main_prints_progress_message(runner: CliRunner, mock_sleep: Mock) -> None:
+    """Test if console.main prints a progress message."""
     result = runner.invoke(console.main, ["--count=1"])
     assert result.output == "Reticulating spline 1...\n"
