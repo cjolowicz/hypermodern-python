29a2dff92d09514595b5d005e1dc4b969a842429
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-13 13:44:43 +0100

    Add docstrings to test suite

--- tests/test_splines.py
+++ tests/test_splines.py
@@ -1,14 +1,17 @@
+"""Test cases for the splines module."""
 from unittest.mock import Mock
 
 from hypermodern_python import splines
 
 
 def test_reticulate_yields_count_times(mock_sleep: Mock) -> None:
+    """Test if reticulate yields <count> times."""
     iterator = splines.reticulate(3)
     assert sum(1 for _ in iterator) == 3
 
 
 def test_reticulate_sleeps(mock_sleep: Mock) -> None:
+    """Test if reticulate sleeps."""
     for _ in splines.reticulate():
         break
     assert mock_sleep.called
