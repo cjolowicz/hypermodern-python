87650547ffa980d3bea8d574053a7afa8616aaa9
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-13 12:49:45 +0100

    Add flake8-bandit to lint session

--- noxfile.py
+++ noxfile.py
@@ -15,7 +15,13 @@ def black(session):
 @nox.session(python=["3.8", "3.7"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-black", "flake8-bugbear", "flake8-import-order")
+    session.install(
+        "flake8",
+        "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-import-order",
+    )
     session.run("flake8", *args)
 
 
