3fef870c0338c2733ef6bfa4dce5321bd05e6d6f
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-13 10:58:51 +0100

    Forward session arguments to pytest

--- noxfile.py
+++ noxfile.py
@@ -3,5 +3,6 @@ import nox
 
 @nox.session(python=["3.8", "3.7"])
 def tests(session):
+    args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov")
+    session.run("pytest", *args)
