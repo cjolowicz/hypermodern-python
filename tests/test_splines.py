347bedb40d3c4a2372a3f9dd3c065bc4bde6b237
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-13 17:04:21 +0100

    Add type annotations for splines module

--- tests/test_splines.py
+++ tests/test_splines.py
@@ -1,12 +1,14 @@
+from unittest.mock import Mock
+
 from hypermodern_python import splines
 
 
-def test_reticulate_yields_count_times(mock_sleep):
+def test_reticulate_yields_count_times(mock_sleep: Mock) -> None:
     iterator = splines.reticulate(3)
     assert sum(1 for _ in iterator) == 3
 
 
-def test_reticulate_sleeps(mock_sleep):
+def test_reticulate_sleeps(mock_sleep: Mock) -> None:
     for _ in splines.reticulate():
         break
     assert mock_sleep.called
