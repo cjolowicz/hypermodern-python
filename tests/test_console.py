733b7e37ec85887a92c614d1845cc6773cb7d1f6
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-13 16:48:12 +0100

    Revert "Use time.sleep mock in console test"
    This reverts commit 0e769b4d079a74e3b6b1b4d4405e28f5b11263c7.

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,6 +9,6 @@ def runner():
     return click.testing.CliRunner()
 
 
-def test_main_succeeds(runner, mock_sleep):
+def test_main_succeeds(runner):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
