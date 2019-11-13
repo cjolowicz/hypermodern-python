79b5569648e89865ef116be5c2cf5a87839ab2c8
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-13 17:03:06 +0100

    Add type annotations for console test

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,26 +1,29 @@
-import click.testing
+from unittest.mock import Mock
+
+from click.testing import CliRunner
 import pytest
+from pytest_mock import MockFixture
 
 from hypermodern_python import console
 
 
 @pytest.fixture
-def runner():
-    return click.testing.CliRunner()
+def runner() -> CliRunner:
+    return CliRunner()
 
 
 @pytest.fixture
-def mock_splines_reticulate(mocker):
+def mock_splines_reticulate(mocker: MockFixture) -> Mock:
     mock = mocker.patch("hypermodern_python.splines.reticulate")
     mock.return_value = [1, 2, 3]
     return mock
 
 
-def test_main_succeeds(runner, mock_splines_reticulate):
+def test_main_succeeds(runner: CliRunner, mock_splines_reticulate: Mock) -> None:
     result = runner.invoke(console.main)
     assert result.exit_code == 0
 
 
-def test_main_prints_progress_message(runner, mock_sleep):
+def test_main_prints_progress_message(runner: CliRunner, mock_sleep: Mock) -> None:
     result = runner.invoke(console.main, ["--count=1"])
     assert result.output == "Reticulating spline 1...\n"
