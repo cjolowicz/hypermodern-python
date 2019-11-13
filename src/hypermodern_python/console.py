1ab4c4eaa2aedbe9affef833b82e101fac3b4c17
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-13 16:06:40 +0100

    Add type annotations to console.main

--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -6,7 +6,7 @@ from . import __version__, splines
 @click.command()
 @click.option("-n", "--count", default=-1, help="Number of splines to reticulate")
 @click.version_option(version=__version__)
-def main(count):
+def main(count: int) -> None:
     """The hypermodern Python project."""
     for spline in splines.reticulate(count):
         click.echo(f"Reticulating spline {spline}...")
