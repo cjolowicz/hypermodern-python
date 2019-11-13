eede0c058a960ddd7021870eef4520c73f273233
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-13 16:06:07 +0100

    Add type annotations to splines.reticulate

--- src/hypermodern_python/splines.py
+++ src/hypermodern_python/splines.py
@@ -1,8 +1,9 @@
 import time
+from typing import Iterator
 
 
-def reticulate(count=-1):
-    spline = 0
+def reticulate(count: int = -1) -> Iterator[int]:
+    spline: int = 0
     while count < 0 or spline < count:
         time.sleep(1)
         spline += 1
