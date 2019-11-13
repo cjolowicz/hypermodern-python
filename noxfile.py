dcbcc3c8018caf2c60f43518101cd0fd8859d0db
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-13 16:14:24 +0100

    Add flake8-annotations to lint session

--- noxfile.py
+++ noxfile.py
@@ -33,6 +33,7 @@ def lint(session):
     install_with_constraints(
         session,
         "flake8",
+        "flake8-annotations",
         "flake8-bandit",
         "flake8-black",
         "flake8-bugbear",
