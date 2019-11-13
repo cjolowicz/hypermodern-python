1acc95a8affb081b0f35ff432f583dfd212a5523
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-13 16:06:40 +0100

    Add type annotations to console.main

--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -15,7 +15,7 @@ from . import __version__, wikipedia
     show_default=True,
 )
 @click.version_option(version=__version__)
-def main(language):
+def main(language: str) -> None:
     """The hypermodern Python project."""
     data = wikipedia.random_page(language=language)
 
