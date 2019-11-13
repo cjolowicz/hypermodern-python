5f7b1670d9cd2989da1b5be4a7cb5b83e6527c76
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-13 14:59:14 +0100

    Add signature to splines.reticulate docstring

--- src/hypermodern_python/splines.py
+++ src/hypermodern_python/splines.py
@@ -4,7 +4,15 @@ from typing import Iterator
 
 
 def reticulate(count: int = -1) -> Iterator[int]:
-    """Reticulate splines."""
+    """Reticulate splines.
+
+    Args:
+        count: Number of splines to reticulate
+
+    Yields:
+        A reticulated spline
+
+    """
     spline: int = 0
     while count < 0 or spline < count:
         time.sleep(1)
