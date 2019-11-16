a10bc121ac185654401c392a89ac3188ec83713d
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-16 13:34:32 +0100

    Add sphinx_autodoc_typehints extension to Sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -3,4 +3,9 @@ project = "hypermodern-python"
 author = "Claudio Jolowicz"
 copyright = f"2019, {author}"
 html_theme = "sphinx_rtd_theme"
-extensions = ["sphinx_rtd_theme", "sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
