e4500e6b46e82f17a48758f4f31809a02d5ae21c
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-16 13:33:22 +0100

    Add autodoc extension to Sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -3,4 +3,4 @@ project = "hypermodern-python"
 author = "Claudio Jolowicz"
 copyright = f"2019, {author}"
 html_theme = "sphinx_rtd_theme"
-extensions = ["sphinx_rtd_theme"]
+extensions = ["sphinx_rtd_theme", "sphinx.ext.autodoc"]
