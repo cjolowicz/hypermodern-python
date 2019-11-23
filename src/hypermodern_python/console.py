c2e0243fedb5c232f77090844dfd1ba2c9797c63
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-23 23:07:41 +0100

    Clean up --serve implementation

--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -8,6 +8,13 @@ import uvicorn
 from . import __version__, splines
 
 
+def _serve(bind):
+    url = urllib.parse.urlsplit(f"//{bind}")
+    host = url.hostname or "127.0.0.1"
+    port = url.port or 8000
+    uvicorn.run("hypermodern_python.app:app", host=host, port=port)
+
+
 async def _reticulate(count):
     async for spline in splines.reticulate(count):
         click.echo(f"Reticulating spline {spline}...")
@@ -15,15 +22,12 @@ async def _reticulate(count):
 
 @click.command()
 @click.option("-n", "--count", default=-1, help="Number of splines to reticulate")
-@click.option("--serve", type=str, help="Run a web server", metavar="HOST:PORT")
+@click.option("--serve", "bind", type=str, help="Run a web server", metavar="HOST:PORT")
 @click.version_option(version=__version__)
-def main(count: int, serve: str) -> None:
+def main(count: int, bind: str) -> None:
     """The hypermodern Python project."""
-    if serve is not None:
-        url = urllib.parse.urlsplit(f"//{serve}")
-        host = url.hostname or "127.0.0.1"
-        port = url.port or 8000
-        return uvicorn.run("hypermodern_python.app:app", host=host, port=port)
+    if bind is not None:
+        return _serve(bind)
 
     coroutine = _reticulate(count)
     asyncio.run(coroutine)
