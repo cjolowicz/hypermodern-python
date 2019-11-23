64d026ffd08e2b864de9ce50336cc6036ec1b6f8
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-11-23 23:18:24 +0100

    Adapt test case to asynchronous implementation

--- tests/test_splines.py
+++ tests/test_splines.py
@@ -4,14 +4,14 @@ from unittest.mock import Mock
 from hypermodern_python import splines
 
 
-def test_reticulate_yields_count_times(mock_sleep: Mock) -> None:
+async def test_reticulate_yields_count_times(mock_sleep: Mock) -> None:
     """Test if reticulate yields <count> times."""
     iterator = splines.reticulate(3)
-    assert sum(1 for _ in iterator) == 3
+    assert sum([1 async for _ in iterator]) == 3
 
 
-def test_reticulate_sleeps(mock_sleep: Mock) -> None:
+async def test_reticulate_sleeps(mock_sleep: Mock) -> None:
     """Test if reticulate sleeps."""
-    for _ in splines.reticulate():
+    async for _ in splines.reticulate():
         break
     assert mock_sleep.called
