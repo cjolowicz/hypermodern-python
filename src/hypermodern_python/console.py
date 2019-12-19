f2b10274f2fee1d44ba8f4afba85f47bba350bfb
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-12-19 16:13:34 +0100

    Print random fact from Wikipedia API

--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -1,5 +1,8 @@
 """Command-line interface for the hypermodern Python project."""
+import textwrap
+
 import click
+import requests
 
 from . import __version__
 
@@ -8,3 +11,10 @@ from . import __version__
 @click.version_option(version=__version__)
 def main() -> None:
     """The hypermodern Python project."""
+    response = requests.get("https://en.wikipedia.org/api/rest_v1/page/random/summary")
+
+    title = response.json()["title"]
+    extract = response.json()["extract"]
+
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))
