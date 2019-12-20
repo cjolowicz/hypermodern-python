81779cfc99c23712e43da7f8667b5d62337c6fb7
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-12-20 11:34:29 +0100

    Add function random_article

--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -1,5 +1,6 @@
 """Command-line interface for the hypermodern Python project."""
 import textwrap
+from typing import Tuple
 
 import click
 import requests
@@ -7,14 +8,26 @@ import requests
 from . import __version__
 
 
-@click.command()
-@click.version_option(version=__version__)
-def main() -> None:
-    """The hypermodern Python project."""
+def random_article() -> Tuple[str, str]:
+    """Return a random article from Wikipedia.
+
+    Returns:
+        The title and extract of the article
+
+    """
     response = requests.get("https://en.wikipedia.org/api/rest_v1/page/random/summary")
 
     title = response.json()["title"]
     extract = response.json()["extract"]
+
+    return title, extract
+
+
+@click.command()
+@click.version_option(version=__version__)
+def main() -> None:
+    """The hypermodern Python project."""
+    title, extract = random_article()
 
     click.secho(title, fg="green")
     click.echo(textwrap.fill(extract))
