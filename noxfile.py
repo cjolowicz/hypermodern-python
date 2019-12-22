7f46785463897335f29e59d04250cba7df14ec9c
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-12-22 11:28:29 +0100

    Add Nox session to run safety

--- noxfile.py
+++ noxfile.py
@@ -41,6 +41,22 @@ def lint(session):
     session.run("flake8", *args)
 
 
+@nox.session(python="3.8")
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        install_with_constraints(session, "safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+
+
 @nox.session(python=["3.8", "3.7"])
 def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e"]
