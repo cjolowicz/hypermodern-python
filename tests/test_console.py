18e3f7d0fb222f7c30a968da85dc390bd7e25d5e
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-12-31 15:28:50 +0100

    Test if console.main invokes requests.get

--- tests/test_console.py
+++ tests/test_console.py
@@ -27,3 +27,8 @@ def test_main_succeeds(runner, mock_requests_get):
 def test_main_prints_title(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert "Lorem Ipsum" in result.output
+
+
+def test_main_invokes_requests_get(runner, mock_requests_get):
+    runner.invoke(console.main)
+    assert mock_requests_get.called
