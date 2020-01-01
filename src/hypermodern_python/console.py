2438a6ac98cfc346106a2f732277caa6943e79b8
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-01-01 21:42:40 +0100

    Raise an exception on HTTP error codes

--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -14,6 +14,7 @@ API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 def main():
     """The hypermodern Python project."""
     with requests.get(API_URL) as response:
+        response.raise_for_status()
         data = response.json()
 
     title = data["title"]
