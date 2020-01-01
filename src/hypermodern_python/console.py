cf91826506f027773185e6e1a68bc8b911b323da
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-01-01 21:41:47 +0100

    Close response and avoid double parse

--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -6,14 +6,18 @@ import requests
 from . import __version__
 
 
+API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+
+
 @click.command()
 @click.version_option(version=__version__)
 def main():
     """The hypermodern Python project."""
-    response = requests.get("https://en.wikipedia.org/api/rest_v1/page/random/summary")
+    with requests.get(API_URL) as response:
+        data = response.json()
 
-    title = response.json()["title"]
-    extract = response.json()["extract"]
+    title = data["title"]
+    extract = data["extract"]
 
     click.secho(title, fg="green")
     click.echo(textwrap.fill(extract))
