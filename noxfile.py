9837cabd151c08987ed0e823d880c11912835f0c
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-01-04 12:50:21 +0100

    Exclude end-to-end tests from Nox session

--- noxfile.py
+++ noxfile.py
@@ -3,6 +3,6 @@ import nox
 
 @nox.session(python=["3.8", "3.7"])
 def tests(session):
-    args = session.posargs or ["--cov"]
+    args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
