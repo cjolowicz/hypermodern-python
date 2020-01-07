4ea0d8c442139da1e360cacf97dee47ccd3987d5
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-01-07 14:33:48 +0100

    Add fixture with wikipedia.random_page mock

--- tests/test_console.py
+++ tests/test_console.py
@@ -10,6 +10,11 @@ def runner():
     return click.testing.CliRunner()
 
 
+@pytest.fixture
+def mock_wikipedia_random_page(mocker):
+    return mocker.patch("hypermodern_python.wikipedia.random_page")
+
+
 def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
