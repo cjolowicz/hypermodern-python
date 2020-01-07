23efb3a4205f5704b5f551534a49eb33937f2ef0
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-01-07 14:29:11 +0100

    Generalize wikipedia.random_page to non-English editions

--- src/hypermodern_python/wikipedia.py
+++ src/hypermodern_python/wikipedia.py
@@ -2,12 +2,14 @@ import click
 import requests
 
 
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+API_URL = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
 
 
-def random_page():
+def random_page(language="en"):
+    url = API_URL.format(language=language)
+
     try:
-        with requests.get(API_URL) as response:
+        with requests.get(url) as response:
             response.raise_for_status()
             return response.json()
     except requests.RequestException as error:
