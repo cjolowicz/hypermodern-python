a5721cfac31131de08a2f6c1fa909e2112bc0335
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-01-07 15:17:49 +0100

    Register custom marker for end-to-end tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
 import pytest
 
 
+def pytest_configure(config):
+    config.addinivalue_line("markers", "e2e: mark as end-to-end test.")
+
+
 @pytest.fixture
 def mock_requests_get(mocker):
     mock = mocker.patch("requests.get")
