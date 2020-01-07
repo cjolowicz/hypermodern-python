8045836320bfd8a5d330b441a3b62651266fcdf5
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-01-07 14:39:21 +0100

    Add option --language to select Wikipedia edition

--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -6,10 +6,18 @@ from . import __version__, wikipedia
 
 
 @click.command()
+@click.option(
+    "--language",
+    "-l",
+    default="en",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
+    show_default=True,
+)
 @click.version_option(version=__version__)
-def main():
+def main(language):
     """The hypermodern Python project."""
-    data = wikipedia.random_page()
+    data = wikipedia.random_page(language=language)
 
     title = data["title"]
     extract = data["extract"]
