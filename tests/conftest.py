f9b6da01a5985981b9eec966109a8fe399a677c4
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-01-07 14:28:12 +0100

    Move requests.get mock to conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.fixture
+def mock_requests_get(mocker):
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolor sit amet",
+    }
+    return mock
--- tests/test_console.py
+++ tests/test_console.py
@@ -10,16 +10,6 @@ def runner():
     return click.testing.CliRunner()
 
 
-@pytest.fixture
-def mock_requests_get(mocker):
-    mock = mocker.patch("requests.get")
-    mock.return_value.__enter__.return_value.json.return_value = {
-        "title": "Lorem Ipsum",
-        "extract": "Lorem ipsum dolor sit amet",
-    }
-    return mock
-
-
 def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
