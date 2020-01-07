fcb46c1a0e60cd81f571f8e569dcd7da34c70afd
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-01-07 06:44:40 +0100

    Test if console.main fails on request errors

--- tests/test_console.py
+++ tests/test_console.py
@@ -38,3 +38,9 @@ def test_main_uses_en_wikipedia_org(runner, mock_requests_get):
     runner.invoke(console.main)
     args, _ = mock_requests_get.call_args
     assert "en.wikipedia.org" in args[0]
+
+
+def test_main_fails_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = Exception("Boom")
+    result = runner.invoke(console.main)
+    assert result.exit_code == 1
