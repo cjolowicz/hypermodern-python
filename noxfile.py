d5e5d47dabab32e7d77065196409cc2395d86ff5
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-01-11 21:26:48 +0100

    Constrain lint session by poetry.lock

--- noxfile.py
+++ noxfile.py
@@ -1,13 +1,28 @@
+import tempfile
+
 import nox
 
 
 locations = "src", "tests", "noxfile.py"
 
 
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
 @nox.session(python=["3.8", "3.7"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8")
+    install_with_constraints(session, "flake8")
     session.run("flake8", *args)
 
 
