3a9511a8a0a016c97111e711168876b4702fdaf4
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-01-15 18:55:57 +0100

    Add type annotations to pytest_configure

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,11 @@
 from unittest.mock import Mock
 
+from _pytest.config import Config
 import pytest
 from pytest_mock import MockFixture
 
 
-def pytest_configure(config):
+def pytest_configure(config: Config) -> None:
     config.addinivalue_line("markers", "e2e: mark as end-to-end test.")
 
 
