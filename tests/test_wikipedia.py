77a1e00d3c42d34d6d33c9ace7a5df460f4f28ed
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-01-15 20:19:54 +0100

    Add type annotations to wikipedia test

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -1,21 +1,23 @@
+from unittest.mock import Mock
+
 import click
 import pytest
 
 from hypermodern_python import wikipedia
 
 
-def test_random_page_uses_given_language(mock_requests_get):
+def test_random_page_uses_given_language(mock_requests_get: Mock) -> None:
     wikipedia.random_page(language="de")
     args, _ = mock_requests_get.call_args
     assert "de.wikipedia.org" in args[0]
 
 
-def test_random_page_returns_page(mock_requests_get):
+def test_random_page_returns_page(mock_requests_get: Mock) -> None:
     page = wikipedia.random_page()
     assert isinstance(page, wikipedia.Page)
 
 
-def test_random_page_handles_validation_errors(mock_requests_get):
+def test_random_page_handles_validation_errors(mock_requests_get: Mock) -> None:
     mock_requests_get.return_value.__enter__.return_value.json.return_value = None
     with pytest.raises(click.ClickException):
         wikipedia.random_page()
