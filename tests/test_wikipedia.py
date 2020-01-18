c19b8980694181c546f4643484a401f79794b0f5
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-01-18 15:05:05 +0100

    Test if wikipedia.random_page returns a Page

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -5,3 +5,8 @@ def test_random_page_uses_given_language(mock_requests_get):
     wikipedia.random_page(language="de")
     args, _ = mock_requests_get.call_args
     assert "de.wikipedia.org" in args[0]
+
+
+def test_random_page_returns_page(mock_requests_get):
+    page = wikipedia.random_page()
+    assert isinstance(page, wikipedia.Page)
