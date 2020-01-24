e57778f300a08b33ec0e7fd29c3257311db10527
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-01-24 16:06:24 +0100

    Add docstrings for console test cases

--- tests/test_console.py
+++ tests/test_console.py
@@ -22,27 +22,32 @@ def mock_wikipedia_random_page(mocker: MockFixture) -> Mock:
 
 
 def test_main_succeeds(runner: CliRunner, mock_requests_get: Mock) -> None:
+    """It exits with a status code of zero."""
     result = runner.invoke(console.main)
     assert result.exit_code == 0
 
 
 @pytest.mark.e2e
 def test_main_succeeds_in_production_env(runner: CliRunner) -> None:
+    """It exits with a status code of zero (end-to-end)."""
     result = runner.invoke(console.main)
     assert result.exit_code == 0
 
 
 def test_main_prints_title(runner: CliRunner, mock_requests_get: Mock) -> None:
+    """It prints the title of the Wikipedia page."""
     result = runner.invoke(console.main)
     assert "Lorem Ipsum" in result.output
 
 
 def test_main_invokes_requests_get(runner: CliRunner, mock_requests_get: Mock) -> None:
+    """It invokes requests.get."""
     runner.invoke(console.main)
     assert mock_requests_get.called
 
 
 def test_main_uses_en_wikipedia_org(runner: CliRunner, mock_requests_get: Mock) -> None:
+    """It uses the English Wikipedia by default."""
     runner.invoke(console.main)
     args, _ = mock_requests_get.call_args
     assert "en.wikipedia.org" in args[0]
@@ -51,6 +56,7 @@ def test_main_uses_en_wikipedia_org(runner: CliRunner, mock_requests_get: Mock) 
 def test_main_uses_specified_language(
     runner: CliRunner, mock_wikipedia_random_page: Mock
 ) -> None:
+    """It uses the specified language edition of Wikipedia."""
     runner.invoke(console.main, ["--language=pl"])
     mock_wikipedia_random_page.assert_called_with(language="pl")
 
@@ -58,6 +64,7 @@ def test_main_uses_specified_language(
 def test_main_fails_on_request_error(
     runner: CliRunner, mock_requests_get: Mock
 ) -> None:
+    """It exits with a non-zero status code if the request fails."""
     mock_requests_get.side_effect = Exception("Boom")
     result = runner.invoke(console.main)
     assert result.exit_code == 1
@@ -66,6 +73,7 @@ def test_main_fails_on_request_error(
 def test_main_prints_message_on_request_error(
     runner: CliRunner, mock_requests_get: Mock
 ) -> None:
+    """It prints an error message if the request fails."""
     mock_requests_get.side_effect = requests.RequestException
     result = runner.invoke(console.main)
     assert "Error" in result.output
