3c5b1754769f3204151d3583edb35392ddf778ab
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-01-25 13:35:43 +0100

    Enable autodoc and napoleon Sphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -5,3 +5,4 @@ from datetime import datetime
 project = "Hypermodern Python"
 author = "Claudio Jolowicz"
 copyright = f"{datetime.now().year}, {author}"
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
