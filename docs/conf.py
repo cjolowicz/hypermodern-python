3ce914692f94f0bef04f0690d3dd9ebe78ef1b1b
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-01-26 06:30:53 +0100

    Enable sphinx-autodoc-typehints Sphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -5,5 +5,5 @@ from datetime import datetime
 project = "Hypermodern Python"
 author = "Claudio Jolowicz"
 copyright = f"{datetime.now().year}, {author}"
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_autodoc_typehints"]
 html_static_path = ["_static"]
