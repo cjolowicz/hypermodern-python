06e17589f9a6f2c418fb26ef519760992630cb12
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-02-02 16:32:30 +0100

    Use importlib.metadata to declare version

--- src/hypermodern_python/__init__.py
+++ src/hypermodern_python/__init__.py
@@ -1,2 +1,11 @@
 """The hypermodern Python project."""
-__version__ = "0.1.0"
+try:
+    from importlib.metadata import version, PackageNotFoundError  # type: ignore
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError  # type: ignore
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
